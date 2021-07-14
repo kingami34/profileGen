66b467ca99b2522b139a7a27ed977c536b12ac0b
Jeff Lynch <jlynch3@uw.edu>
2021-07-13 20:15:25 -0700

    first commit

--- index.js
+++ index.js
@@ -2,32 +2,55 @@
 const fs =require("fs");
 const inquirer = require("inquirer")
 //Questions for different types of employees
+function manager(){
+ inquirer.prompt([
+    {
+        type: 'input',
+        message: 'What is your name?',
+        name: 'name'
+
+    },
+    {
+        type: 'input',
+        message: 'What is the employee ID?',
+        name: 'ID'
+
+    },
+    {
+        type: 'input',
+        message: 'What is the email address?',
+        name: 'email'
+
+    },
+    {
+        type: 'input',
+        message: 'What is the office number?',
+        name: 'office'
+
+    },
+    //Creating a new instance
+ ]) .then(answers =>{
+     const manager = new Manager(answers.name , answers.ID , answers.email , answers.office)
+ })
+}
+
+function engineer(){
+
+}
+function intern(){
+
+}
+function menuAsk(){
+
+}
+function buildTeam(){
+
+}
+
+
 inquirer
     .prompt([
-        {
-            type: 'input',
-            message: 'What is your name?',
-            name: 'Manager'
-
-        },
-        {
-            type: 'input',
-            message: 'What is the employee ID?',
-            name: 'Manager'
-
-        },
-        {
-            type: 'input',
-            message: 'What is the email address?',
-            name: 'Manager'
-
-        },
-        {
-            type: 'input',
-            message: 'What is the office number?',
-            name: 'Manager'
-
-        },
+       
         {
             type: 'input',
             message: 'What is your name?',
